175817db05e0a4b34f9446acfe3e77f629ed3629
nakajima-masahiko <nakajimamasahiko14@gmail.com>
2026-04-14 23:12:12 +0900

    Configure GitHub Pages deployment for Vite SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ExperimentPage from './pages/ExperimentPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/experiments/:id" element={<ExperimentPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
